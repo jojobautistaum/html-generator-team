a793a93b1a809ae7ff35b153363d72d7865b6f7b
Jojo Bautista <gjojob@yahoo.com>
2021-12-17 00:36:15 -0600

    adjusted indentation so generated html file has proper indentation

--- src/html-template.js
+++ src/html-template.js
@@ -3,49 +3,49 @@ const Manager = require('../lib/Manager');
 const Intern = require('../lib/Intern');
 
 function htmlPage(manager, engineer, intern) {
-    return `
-      <!DOCTYPE html>
-      <html lang="en">
-      <head>
-      <meta charset="UTF-8">
-      <meta http-equiv="X-UA-Compatible" content="IE=edge">
-      <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" />
-      <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css" />
-      <link rel="stylesheet" href="./style.css">
-      <title>My Team Page</title>
-      </head>
-      <body>
-        <header>
-          <h1>My Team</h1>
-        </header>
-        <main class="container">
-          <section class="row">
-            ${managerSection(manager)}
-            ${engineerSection(engineer)}
-            ${internSection(intern)}
-          </section>
-        </main>
-      </body>
-      </html>
-    `;
-  };
+  return `
+  <!DOCTYPE html>
+  <html lang="en">
+  <head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" />
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css" />
+  <link rel="stylesheet" href="./style.css">
+  <title>My Team Page</title>
+  </head>
+  <body>
+    <header>
+      <h1>My Team</h1>
+    </header>
+    <main class="container">
+      <section class="row">
+        ${managerSection(manager)}
+        ${engineerSection(engineer)}
+        ${internSection(intern)}
+      </section>
+    </main>
+  </body>
+  </html>
+  `;
+};
 
 function managerSection(manager) {
   const managerArr = new Manager(manager[0].name, manager[0].id, manager[0].email, manager[0].officeNumber);
   return `
-    <div class="col-sm-4">
-      <div class="card">
-        <h4>${managerArr.getName()}<br/>
-        <i class="fas fa-mug-hot"></i> ${managerArr.getRole()} 
-        </h4>
-        <div class="card-body">
-          <p>ID: ${managerArr.getId()}</p>
-          <p>Email: ${managerArr.getEmail()}</p>
-          <p>Office Number: ${managerArr.getOfficeNumber()}</p>
+        <div class="col-sm-4">
+          <div class="card">
+            <h4>${managerArr.getName()}<br/>
+            <i class="fas fa-mug-hot"></i> ${managerArr.getRole()} 
+            </h4>
+            <div class="card-body">
+              <p>ID: ${managerArr.getId()}</p>
+              <p>Email: ${managerArr.getEmail()}</p>
+              <p>Office Number: ${managerArr.getOfficeNumber()}</p>
+            </div>
+          </div>
         </div>
-      </div>
-    </div>
   `;
 };
 
@@ -55,19 +55,19 @@ function engineerSection(engineer) {
     const engineerArr = new Engineer(engineer[i].name, engineer[i].id, engineer[i].email, engineer[i].github)
     engineers = engineers + 
     `
-    <div class="col-sm-4">
-      <div class="card">
-        <h4>${engineerArr.getName()}<br/>
-        <i class="fas fa-glasses"></i> ${engineerArr.getRole()}
-        </h4>
-        <div class="card-body">
-          <p>ID: ${engineerArr.getId()}</p>
-          <p>Emal: ${engineerArr.getEmail()}</p>
-          <p>GitHub: ${engineerArr.getGithub()}</p>
+        <div class="col-sm-4">
+          <div class="card">
+            <h4>${engineerArr.getName()}<br/>
+            <i class="fas fa-glasses"></i> ${engineerArr.getRole()}
+            </h4>
+            <div class="card-body">
+              <p>ID: ${engineerArr.getId()}</p>
+              <p>Emal: ${engineerArr.getEmail()}</p>
+              <p>GitHub: ${engineerArr.getGithub()}</p>
+            </div>
+          </div>
         </div>
-      </div>
-    </div>
-    `;
+`;
   }
   return engineers;
 };
@@ -78,18 +78,18 @@ function internSection(intern) {
     const internArr = new Intern(intern[i].name, intern[i].id, intern[i].email, intern[i].school)
     interns = interns + 
     `
-    <div class="col-sm-4">
-      <div class="card">
-        <h4>${internArr.getName()}<br/>
-        <i class="fas fa-user-graduate"></i> ${internArr.getRole()}
-        </h4>
-        <div class="card-body">
-          <p>ID: ${internArr.getId()}</p>
-          <p>Emal: ${internArr.getEmail()}</p>
-          <p>School: ${internArr.getSchool()}</p>
+        <div class="col-sm-4">
+          <div class="card">
+            <h4>${internArr.getName()}<br/>
+            <i class="fas fa-user-graduate"></i> ${internArr.getRole()}
+            </h4>
+            <div class="card-body">
+              <p>ID: ${internArr.getId()}</p>
+              <p>Emal: ${internArr.getEmail()}</p>
+              <p>School: ${internArr.getSchool()}</p>
+            </div>
+          </div>
         </div>
-      </div>
-    </div>
     `;
   }
   return interns;
